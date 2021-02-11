98f35a43ecfc604181444c1ab841ba0842082285
Liam Nelski <ljnelski@gmail.com>
2021-02-11 13:04:18 -0500

    Finished Pathfinding

---
+++ Assets/_Scripts/AIRadius.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIRadius : MonoBehaviour
+{
+    public float radius;
+}
--- Assets/_Scripts/DinosaurController.cs
+++ Assets/_Scripts/DinosaurController.cs
@@ -5,22 +5,28 @@ using UnityEngine.AI;
 
 public class DinosaurController : MonoBehaviour
 {
-    public Transform targetDestination;
+    public Transform village;
+    public List<string> targetPriority;
+    public float stopDis;
 
     private NavMeshAgent navAgent;
+    private TargetFinder targetFinder;
+    private Transform currentTarget;
     private void Awake()
     {
         navAgent = GetComponent<NavMeshAgent>();
+        targetFinder = GetComponent<TargetFinder>();        
     }
-
     void Start()
     {
-        
+        village = GameObject.Find("Village").transform;
     }
     // Update is called once per frame
     void Update()
     {
-        navAgent.SetDestination(targetDestination.position);
+        currentTarget = DetermineTarget();
+        navAgent.stoppingDistance = FindStopingDistance();
+        navAgent.SetDestination(currentTarget.position);
     }
     private void Attack()
     {
@@ -29,8 +35,47 @@ public class DinosaurController : MonoBehaviour
          2. Send signal to do damage to target
          3. Check for higher priority target0
 
-        Make whole script responsable for finding targets
+        Make whole script responsable determining target
          */
        
+    }
+    private Transform DetermineTarget()
+    {
+        Transform newTarget = village;
+        // Loop through avalible targets
+        for (int i = 0; i < targetFinder.visableTargets.Count; i++)
+        {
+            //loop thorugh avalible tags
+            for (int k = targetPriority.Count - 1; k > -1 ; k--)
+            {
+                // check if target tag = priority tag;
+                if (targetFinder.visableTargets[i].CompareTag(targetPriority[k]))
+                {
+                    // check if selected newTarget and potenial newTargets is closer
+                    if (newTarget != null && newTarget.CompareTag(targetFinder.visableTargets[i].tag))
+                    {
+                        float dstNewTarget = Vector3.Distance(transform.position, newTarget.position);
+                        float dstPontenialNewTarget = Vector3.Distance(transform.position, targetFinder.visableTargets[i].position);
+                        if (dstNewTarget > dstPontenialNewTarget)
+                        {
+                            newTarget = targetFinder.visableTargets[i];
+                        }
+                    }
+                    else
+                    {
+                        newTarget = targetFinder.visableTargets[i];
+                    }
+                    break;
+                }
+            }
+        }
+        return newTarget;
+    }
+    private float FindStopingDistance()
+    {
+        if (currentTarget == village)
+            return stopDis;
+        else
+            return currentTarget.GetComponent<AIRadius>().radius + stopDis;
     }
 }
