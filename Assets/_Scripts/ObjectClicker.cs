9d17eff416024549f56d43d61238a19acc5cc8c1
Liam Nelski <ljnelski@gmail.com>
2021-02-25 15:04:25 -0500

    First pass of Cyberman Animations

--- Assets/_Scripts/ObjectClicker.cs
+++ Assets/_Scripts/ObjectClicker.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class ObjectClicker : MonoBehaviour
 {
     public LayerMask clickableMask;
+    public string cameraName;
+
+    private Camera camera;
+    private void Awake()
+    {
+        camera = GameObject.Find(cameraName).GetComponent<Camera>();
+    }
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit, 100f, clickableMask))
             {
                 Debug.Log("Adding Resource Task");
