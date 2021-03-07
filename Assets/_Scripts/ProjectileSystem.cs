56f560abe4455d6be2ef52d587fcbaba9e523615
Chloe <chloe_baker@live.com>
2021-03-07 18:43:02 -0500

    Projectile Sys v2, Tower Setup
    - changed the projectile scripts so that projectiles track and correctly rotate
    - gave BEnemy, MEnemy, SEnemy tags and Enemy layer to dinos
    - spearman tower prefab has Tower_Cyberman as a child
    - projectiles shoot from the cyberman's hand (sort of)
    - throw animation plays when shooting
    - Tower_Cyberman will rotate to look at target

--- Assets/_Scripts/ProjectileSystem.cs
+++ Assets/_Scripts/ProjectileSystem.cs
@@ -8,6 +8,9 @@ public class ProjectileSystem : MonoBehaviour
     [SerializeField] private Transform shootFrom; //where projectile shoots from
     [SerializeField] private float fireRate = 1f; //how often to shoot (in seconds)
     [SerializeField] private float moveSpeed = 15f; //how fast projectile goes
+    
+    [SerializeField] private GameObject TowerMan; //Tower_Cyberman
+    private Animator anim; //Tower_Cyberman animator
     public List<string> targetPriority; //list of enemies
 
     private TargetFinder targetFinder; //use the target finder
@@ -17,6 +20,7 @@ public class ProjectileSystem : MonoBehaviour
 
     void Awake()
     {
+        anim = TowerMan.GetComponent<Animator>();
         targetFinder = GetComponent<TargetFinder>();
         nextSpawnTime += Time.time + fireRate;
     }
@@ -27,17 +31,27 @@ public class ProjectileSystem : MonoBehaviour
 
         if (!currentTarget.CompareTag("Tower")) //if targeting tower do nothing
         {
+            // Rotate Cyberman
+            Vector3 lookDir = (currentTarget.position - transform.position);
+            TowerMan.transform.rotation = Quaternion.LookRotation(lookDir);
+
             Debug.Log("Shoot--");
             if (Time.time > nextSpawnTime) //only instantiate new projectile every fireRate increment
             {
-                // Instantiate at the shootFrom position and zero rotation.
-                Instantiate(projectilePrefab, new Vector3(shootFrom.position.x, shootFrom.position.y, shootFrom.position.z), Quaternion.identity);
+                anim.Play("Base Layer.Cyberman_Throw", 0, 0.95f); // Play the Cyberman's throw animation
+                
+                if (!anim.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.Cyberman_Throw"))
+                {
+                    // Instantiate at the shootFrom position and zero rotation.
+                    Instantiate(projectilePrefab,
+                        new Vector3(shootFrom.position.x, shootFrom.position.y, shootFrom.position.z), Quaternion.identity);
+                }
 
                 nextSpawnTime += fireRate;
             }
 
             Vector3 shootDir = (currentTarget.position - transform.position).normalized;
-            projectilePrefab.GetComponent<Projectile>().Setup(shootDir, moveSpeed); //add force to the projectile
+            projectilePrefab.GetComponent<Projectile>().Setup(shootDir, moveSpeed, currentTarget); //add force to the projectile
         }
     }
 
