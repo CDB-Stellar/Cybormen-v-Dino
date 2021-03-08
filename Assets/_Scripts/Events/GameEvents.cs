0449c96a7a5513a4d5c8452399bf49adabac5a2d
Liam Nelski <ljnelski@gmail.com>
2021-03-08 11:29:14 -0500

    First pass of Needing resources to build

--- Assets/_Scripts/Events/GameEvents.cs
+++ Assets/_Scripts/Events/GameEvents.cs
@@ -9,12 +9,32 @@ public class GameEvents : MonoBehaviour
     private void Awake()
     {
         current = this;
-    } 
+    }
 
+    public EventHandler<BuildRequestArgs> OnBuildRequest;
     public Action<ResourceType, float> OnIncrementResource;
     
     public void IncrementResource(ResourceType resource, float amount)
     {
         OnIncrementResource?.Invoke(resource, amount);
+    }
+    public void CanBuildRequest(object sender, BuildRequestArgs e)
+    {
+        if (PlayerResources.Wood >= e.WoodCost && PlayerResources.Stone >= e.StoneCost && PlayerResources.Iron >= e.IronCost && PlayerResources.Electronics >= e.ElectronicsCost)
+        {
+            Debug.Log("Build Request Event says enough resources");
+
+            IncrementResource(ResourceType.Wood, -e.WoodCost);
+            IncrementResource(ResourceType.Stone, -e.StoneCost);
+            IncrementResource(ResourceType.Iron, -e.IronCost);
+            IncrementResource(ResourceType.Electronics, -e.ElectronicsCost);
+
+            e.CanBuild = true;
+        }
+        else
+        {
+            Debug.Log("Build Request Event says not enough resources");
+            e.CanBuild = false;
+        }
     }
 }
