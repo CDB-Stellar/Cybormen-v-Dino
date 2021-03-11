28f450041fae2c6fafc18e77eb65781d477a19d0
Liam Nelski <ljnelski@gmail.com>
2021-03-10 20:39:22 -0500

    I don't remember what I was doing

---
+++ Assets/_Scripts/UI Scripts/Messages.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Messages : MonoBehaviour
+{
+    public static Messages current;
+
+    public GameObject messagePrefab;
+
+    public void ShowMessage(string msg)
+    {
+
+    }
+    public void ShowResourceNotification(string msg, ResourceType resource)
+    {
+
+    }
+}
