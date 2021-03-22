294059d86dfd7e92da3b287aeec745f11191e032
Liam Nelski <ljnelski@gmail.com>
2021-03-21 21:38:30 -0400

    First stages of Mobilaiztion

--- Assets/_Scripts/CameraController.cs
+++ Assets/_Scripts/CameraController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField] float movementSpeed;
-    [SerializeField] float rotationSpeed;
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float rotationSpeed;
+    public GameObject joystick;
+
+    private FixedJoystick control;
     // Start is called before the first frame update
     // Update is called once per frame
+    private void Awake()
+    {
+        control = joystick.GetComponent<FixedJoystick>();
+    }
     void Update()
     {
+        /*
         float horiz = Input.GetAxis("Horizontal");
         float vert = Input.GetAxis("Vertical");
         float rot = Input.GetAxis("Rotation");
+        */
+
+        float horiz = control.Horizontal;
+        float vert = control.Vertical;
 
         if (horiz > 0.1f || horiz < -0.1f)
         {
@@ -22,10 +34,10 @@ public class CameraController : MonoBehaviour
         {
             transform.Translate(Vector3.forward * vert * movementSpeed);
         }
-        if (rot > 0.1f || rot < -0.1f)
-        {
-            transform.Rotate(new Vector3(0f, rotationSpeed * rot, 0f), Space.World);
-        }
+        //if (rot > 0.1f || rot < -0.1f)
+        //{
+        //    transform.rotate(new vector3(0f, rotationspeed * rot, 0f), space.world);
+        //}
 
 
     }
